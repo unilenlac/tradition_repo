de36d94e3b0ee2cc2c5ff48b831818520fed46a2
Renato <renato.diaz@unil.ch>
2023-06-15 13:23:49 +0200

    upgrade pom/dockerfile:
    updating the pom.xml form the Violeta Seretan version (1.1)
    
    - install as standalone dependency the com.alexmerz.graphviz deprecated library
    - include java.util.Arrays in the VariantGraphTest.java that was preventing build
    - add /lib/com.alexmerz.jar in code base
    
    that version can be builded and deployed in java8 tomcat9 context

--- src/test/java/net/stemmaweb/stemmaserver/integrationtests/VariantGraphServiceTest.java
+++ src/test/java/net/stemmaweb/stemmaserver/integrationtests/VariantGraphServiceTest.java
@@ -16,6 +16,7 @@ import org.neo4j.test.TestGraphDatabaseFactory;
 import javax.ws.rs.core.Response;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
